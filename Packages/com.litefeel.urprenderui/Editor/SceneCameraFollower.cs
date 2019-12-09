6d74aab67009c3c765923f33f831b5cf4b917b21
litefeel <litefeel@gmail.com>
2019-12-09 23:16:31 +0800

    Update Render UI

---
+++ Packages/com.litefeel.urprenderui/Editor/SceneCameraFollower.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace URPRenderUI.Editor
+{
+    public class SceneCameraFollower : EditorWindow
+    {
+        private Camera m_TargetCamera;
+        private bool m_Lock = true;
+
+        private void OnGUI()
+        {
+            m_TargetCamera = (Camera)EditorGUILayout.ObjectField("TargetCamera", m_TargetCamera, typeof(Camera), true);
+            m_Lock = EditorGUILayout.Toggle("Lock Scene Camera", m_Lock);
+        }
+
+        private void OnEnable()
+        {
+            if (m_TargetCamera == null)
+            {
+                var canvas = GameObject.FindObjectOfType<Canvas>();
+                if (canvas != null)
+                    m_TargetCamera = canvas.worldCamera;
+            }
+
+            SceneView.duringSceneGui -= OnSceneGUI;
+            SceneView.duringSceneGui += OnSceneGUI;
+        }
+
+        private void OnDisable()
+        {
+            SceneView.duringSceneGui -= OnSceneGUI;
+        }
+
+        private void OnSceneGUI(SceneView sceneView)
+        {
+            if (!m_TargetCamera || !m_Lock) return;
+
+            var sceneCamera = sceneView.camera;
+            sceneCamera.transform.position = m_TargetCamera.transform.position;
+            sceneCamera.transform.rotation = m_TargetCamera.transform.rotation;
+            SceneView.lastActiveSceneView.LookAtDirect(m_TargetCamera.transform.position, m_TargetCamera.transform.rotation);
+        }
+
+
+        [MenuItem("Window/Scene Camera Follwer")]
+        private static void Init()
+        {
+            GetWindow<SceneCameraFollower>().Show();
+        }
+    }
+}
---
+++ Packages/com.litefeel.urprenderui/Runtime/UICameraSync.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace URPRenderUI
+{
+    [ExecuteAlways]
+    [DisallowMultipleComponent]
+    public class UICameraSync : MonoBehaviour
+    {
+        private Camera m_UICamera;
+        private Camera m_MainCamera;
+        [SerializeField]
+        private Canvas m_Canvas;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_UICamera = GetComponent<Camera>();
+            if (m_Canvas == null)
+                m_Canvas = FindObjectOfType<Canvas>();
+            m_MainCamera = m_Canvas.worldCamera;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var frustumHeight = 2.0f * m_Canvas.planeDistance * Mathf.Tan(m_MainCamera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+            m_UICamera.orthographicSize = frustumHeight / 2f;
+        }
+    }
+}
